38e05c450152a893a0c91d206393a8a536478a43
armanrakib123 <armanrakib61@gmail.com>
2025-10-02 00:07:58 +0600

    code_1

---
+++ Code/virtual_doctor/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.amazonaws.com',
+                port: '',
+                pathname: '/my-bucket/**',
+                search: '',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
---
+++ Code/virtual_doctor/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 0.3s ease-out',
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light", "night"], 
+  },
+};
